6d8067c94ea08befdb30d3dd8b68c516d2b8fd41
llmvetter <lennart.vetter@yahoo.de>
2023-11-30 18:46:57 +0100

    update fucntionality, however throws a CORS error

--- client/src/components/Patient.vue
+++ client/src/components/Patient.vue
@@ -3,7 +3,7 @@
       <div class="row">
         <div class="col-sm-10">
           <h1>Patient Dashboard</h1>
-          <hr><br><br>
+          <br><br>
           <button type="button" class="btn btn-success btn-sm" @click="toggleAddBookModal">Add Patient</button>
           <br><br>
           <table class="table table-hover">
@@ -22,7 +22,7 @@
                 <td>{{ patient.more_info }}</td>
                 <td>
                   <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-warning btn-sm">Update</button>
+                    <button type="button" class="btn btn-warning btn-sm" @click="toggleEditBookModal(book)">Update</button>
                     <button type="button" class="btn btn-danger btn-sm">Delete</button>
                   </div>
                 </td>
@@ -101,6 +101,74 @@
       </div>
     </div>
   </div>
+  <!-- edit book modal -->
+  <div
+    ref="editBookModal"
+    class="modal fade"
+    :class="{ show: activeEditBookModal, 'd-block': activeEditBookModal }"
+    tabindex="-1"
+    role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Update</h5>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+            @click="toggleEditBookModal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="editPatientName" class="form-label">Full Name:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="editPatientName"
+                v-model="editPatientForm.full_name"
+                placeholder="Enter Patient Full Name">
+            </div>
+            <div class="mb-3">
+              <label for="editPatientStatus" class="form-label">Status:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="editBookAuthor"
+                v-model="editPatientForm.status"
+                placeholder="Enter Status">
+            </div>
+            <div class="mb-3">
+              <label for="editPatientMoreInfo" class="form-label">More Info</label>
+              <input
+                type="text"
+                class="form-control"
+                id="editPatientMoreInfo"
+                v-model="editPatientForm.more_info"
+                placeholder="Enter more Patient Info">
+            </div>
+            <div class="btn-group" role="group">
+              <button
+                type="button"
+                class="btn btn-primary btn-sm"
+                @click="handleEditSubmit">
+                Submit
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger btn-sm"
+                @click="handleEditCancel">
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
   
 <script>
@@ -111,6 +179,14 @@ export default {
     return {
       activeAddBookModal: false,
       addPatientForm: {
+        id: '',
+        full_name: '',
+        status: '',
+        more_info: '',
+      },
+      activeEditBookModal: false,
+      editPatientForm: {
+        id: '',
         full_name: '',
         status: '',
         more_info: '',
@@ -159,6 +235,10 @@ export default {
       this.addPatientForm.full_name = '';
       this.addPatientForm.status = '';
       this.addPatientForm.more_info = '';
+      this.editPatientForm.id = '';
+      this.editPatientForm.full_name = '';
+      this.editPatientForm.status = '';
+      this.editPatientForm.more_info = '';
     },
     toggleAddBookModal() {
       const body = document.querySelector('body');
@@ -168,6 +248,45 @@ export default {
       } else {
         body.classList.remove('modal-open');
       }
+    },
+    toggleEditBookModal(patient) {
+      if (patient) {
+        this.editPatientForm = patient;
+      }
+      const body = document.querySelector('body');
+      this.activeEditBookModal = !this.activeEditBookModal;
+      if (this.activeEditBookModal) {
+        body.classList.add('modal-open');
+      } else{
+        body.classList.remove('modal-open');
+      }
+    },
+    handleEditSubmit() {
+      this.toggleEditBookModal(null);
+      let read = false;
+      if (this.editPatientForm.read) read = true;
+      const payload = {
+        full_name: this.editPatientForm.full_name,
+        status: this.editPatientForm.status,
+        more_info: this.editPatientForm.more_info,
+      };
+      this.updatePatient(payload, this.editPatientForm.id);
+    },
+    updatePatient(payload, patientID) {
+      const path = `http://localhost:5000/patients/${patientID}`;
+      axios.put(path, payload)
+        .then(() => {
+          this.getPatient();
+        })
+        .catch((error) => {
+          console.error(error);
+          this.getPatient();
+        });
+    },
+    handleEditCancel() {
+      this.toggleEditBookModal(null);
+      this.initForm();
+      this.getPatient();
     },
   },
   created() {
--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
 
 
 # instantiate the app
@@ -7,17 +8,20 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 Patients = [
-    {
+    {   
+        'id': uuid.uuid4().hex,
         'full_name': 'Jonas P',
         'status': 'inactive',
         'more_info': 'tbd'
     },
-    {
+    {   
+        'id': uuid.uuid4().hex,
         'full_name': 'Jonas P',
         'status': 'inactive',
         'more_info': 'tbd'
     },
-    {
+    {   
+        'id': uuid.uuid4().hex,
        'full_name': 'Jonas P',
         'status': 'inactive',
         'more_info': 'tbd'
@@ -28,6 +32,13 @@ Patients = [
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+# helper function
+def remove_patient(patient_id):
+    for patient in Patients:
+        if patient['id'] == patient_id:
+            Patients.remove(patient)
+            return True
+    return False
 
 # sanity check route
 @app.route('/strokecockpit', methods=['GET'])
@@ -40,6 +51,7 @@ def all_patients():
     if request.method == 'POST':
         post_data = request.get_json()
         Patients.append({
+            'id': uuid.uuid4().hex,
             'full_name': post_data.get('full_name'),
             'status': post_data.get('status'),
             'more_info': post_data.get('more_info')
@@ -47,6 +59,21 @@ def all_patients():
         response_object['message'] = 'Patient added!'
     else: 
         response_object['patients'] = Patients
+    return jsonify(response_object)
+
+@app.route('/patients/<patient_id>', methods=['PUT'])
+def single_patient(patient_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_patient(patient_id)
+        Patients.append({
+            'id': uuid.uuid4().hex,
+            'full_name': post_data.get('full_name'),
+            'status': post_data.get('status'),
+            'more_info': post_data.get('more_info')
+        })
+        response_object['message'] = 'Patient updated!'
     return jsonify(response_object)
 
 if __name__ == '__main__':
