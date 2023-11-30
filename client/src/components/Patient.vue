9d481aea0a63628e75c9d8ee415aef91ba8986bf
llmvetter <lennart.vetter@yahoo.de>
2023-11-30 16:16:02 +0100

    added POST functionality

--- client/src/components/Patient.vue
+++ client/src/components/Patient.vue
@@ -4,7 +4,7 @@
         <div class="col-sm-10">
           <h1>Patient Dashboard</h1>
           <hr><br><br>
-          <button type="button" class="btn btn-success btn-sm">Add Patient</button>
+          <button type="button" class="btn btn-success btn-sm" @click="toggleAddBookModal">Add Patient</button>
           <br><br>
           <table class="table table-hover">
             <thead>
@@ -32,31 +32,150 @@
         </div>
       </div>
     </div>
-  </template>
+
+  <!-- add new book modal -->
+  <div
+    ref="addBookModal"
+    class="modal fade"
+    :class="{ show: activeAddBookModal, 'd-block': activeAddBookModal }"
+    tabindex="-1"
+    role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Add a new patient</h5>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+            @click="toggleAddBookModal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="addPatientFullname" class="form-label">Full Name:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="addPatientFullname"
+                v-model="addPatientForm.full_name"
+                placeholder="Enter patient full name">
+            </div>
+            <div class="mb-3">
+              <label for="addPatientStatus" class="form-label">Status:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="addBookAuthor"
+                v-model="addPatientForm.status"
+                placeholder="Enter patient status">
+            </div>
+            <div class="mb-3">
+              <label for="addPatientMoreInfo" class="form-label">More Info:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="addPatientMoreInfo"
+                v-model="addPatientForm.more_info"
+                placeholder="Enter further patient info">
+            </div>
+            <div class="btn-group" role="group">
+              <button
+                type="button"
+                class="btn btn-primary btn-sm"
+                @click="handleAddSubmit">
+                Submit
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger btn-sm"
+                @click="handleAddReset">
+                Reset
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
   
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        patients: [],
-      };
-    },
-    methods: {
-      getPatients() {
-        const path = 'http://localhost:5000/patients';
-        axios.get(path)
-          .then((res) => {
-            this.patients = res.data.patients;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      activeAddBookModal: false,
+      addPatientForm: {
+        full_name: '',
+        status: '',
+        more_info: '',
       },
+      patients: [],
+    };
+  },
+  methods: {
+    addBook(payload) {
+      const path = 'http://localhost:5000/patients';
+      axios.post(path, payload)
+        .then(() => {
+          this.getPatient();
+        })
+        .catch((error) => {
+
+          console.log(error);
+          this.getPatient();
+        });
     },
-    created() {
-      this.getPatients();
+    getPatient() {
+      const path = 'http://localhost:5000/patients';
+      axios.get(path)
+        .then((res) => {
+          this.patients = res.data.patients;
+        })
+        .catch((error) => {
+
+          console.error(error);
+        });
     },
-  };
-  </script>
+    handleAddReset() {
+      this.initForm();
+    },
+    handleAddSubmit() {
+      this.toggleAddBookModal();
+      let read = false;
+      if (this.addPatientForm.read[0]) {
+        read = true;
+      }
+      const payload = {
+        full_name: this.addPatientForm.full_name,
+        status: this.addPatientForm.status,
+        more_info: this.addPatientForm.more_info
+      };
+      this.addPatient(payload);
+      this.initForm();
+    },
+    initForm() {
+      this.addPatientForm.full_name = '';
+      this.addPatientForm.status = '';
+      this.addPatientForm.more_info = '';
+    },
+    toggleAddBookModal() {
+      const body = document.querySelector('body');
+      this.activeAddBookModal = !this.activeAddBookModal;
+      if (this.activeAddBookModal) {
+        body.classList.add('modal-open');
+      } else {
+        body.classList.remove('modal-open');
+      }
+    },
+  },
+  created() {
+    this.getPatient();
+  },
+};
+</script>
--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 
@@ -34,12 +34,22 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 def ping_pong():
     return jsonify('This is the Strokecockpit Landing page')
 
-@app.route('/patients', methods=['GET'])
+@app.route('/patients', methods=['GET', 'POST'])
 def all_patients():
-    return jsonify({
-        'status': 'success',
-        'patients': Patients
-    })
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        Patients.append({
+            'full_name': post_data.get('full_name'),
+            'status': post_data.get('status'),
+            'more_info': post_data.get('more_info')
+        })
+        response_object['message'] = 'Patient added!'
+    else: 
+        response_object['patients'] = Patients
+    return jsonify(response_object)
 
 if __name__ == '__main__':
     app.run()
+
+
